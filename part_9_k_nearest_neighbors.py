5f936c4c0ae31a4e990521530d8b013f003bf0e0
KevinH <khui98@gmail.com>
2026-02-11 13:47:57 -0500

    Create part_9_k_nearest_neighbors.py

---
+++ part_9_k_nearest_neighbors.py
@@ -0,0 +1,79 @@
+# Part 9 - K-Nearest Neighbors (KNN)
+
+# Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Import the dataset
+dataset = pd.read_csv('datasets/Social_Network_Ads.csv')
+
+# Separates columns for processing
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Split dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Train KNN model on training set
+knn = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+knn = knn.fit(X_train, y_train)
+
+# Predicting a new result
+print(knn.predict(sc.transform([[30, 87000]])))
+
+# Predicting the test set results
+y_pred = knn.predict(X_test)
+array_reshaped = np.concatenate((y_pred.reshape(len(y_pred), 1), (y_test.reshape(len(y_test), 1))), axis=1)
+print(array_reshaped)
+
+# Making the confusion matrix to evaluate accuracy of the model
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
+
+# Visualsing the training set results
+plt.figure('K-Nearest Neighbor (Training set)')
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=1),
+                     np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=1))
+plt.contourf(X1, X2, knn.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1], c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('K-Nearest Neighbor (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.legend()
+
+# Visualising the test set results
+plt.figure('K-Nearest Neighbor (Test set)')
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 10, stop=X_set[:, 0].max() + 10, step=1),
+                     np.arange(start=X_set[:, 1].min() - 1000, stop=X_set[:, 1].max() + 1000, step=1))
+plt.contourf(X1, X2, knn.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set==j, 0], X_set[y_set==j, 1], c=ListedColormap(('red', 'green'))(i), label=j)
+plt.title('K-Nearest Neighbor (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+
+# Show plots
+plt.show()
